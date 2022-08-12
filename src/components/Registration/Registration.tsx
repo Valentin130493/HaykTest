c2bcaf4231c53e1034fed01deab3a5bfdc08dcd3
Valentin130493 <gerasimenko130493@gmail.com>
2022-08-12 14:55:57 +0300

    create a project sturture

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { TextField } from "@mui/material";
 import { Link } from "react-router-dom";
-import { routs } from "../../AppRouts/сonstants/сonstants";
+import { routs } from "../../routs/сonstants/сonstants";
 
 interface IFormInput {
   Email: string;
